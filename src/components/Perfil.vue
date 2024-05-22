3504fb50585c24e299793ce450cd4a24abef09cc
Mateus Maranhão <feradourada@gmail.com>
2024-05-22 13:19:22 -0300

    Implement Perfil.vue

--- src/components/Perfil.vue
+++ src/components/Perfil.vue
@@ -1,6 +1,32 @@
 <template>
     <nav-usuario />
-    <h1>Perfil</h1>
+    <div class="container">
+        <div class="row justify-content-center align-items-center vh-100">
+            <div class="card card-forms">
+                <div class="card-body">
+                    <h5 class="card-title">Seu perfil</h5>
+                    <form @submit.prevent="">
+                        <div class="row mb-4">
+                            <div class="col-md-6">
+                                <label for="name" class="form-label">Nome</label>
+                                <input type="text" disabled class="form-control" name="nome"
+                                    id="nome" placeholder="Mateus">
+                            </div>
+                            <div class="col-md-6">
+                                <label for="email" class="form-label">Email</label>
+                                <input type="text" disabled class="form-control"
+                                    name="email" id="email" placeholder="mateusnmaranhao@gmail.com">
+                            </div>
+                        </div>
+                        <div class="d-flex justify-content-end mt-4">
+                            <button @click.prevent="" type="submit"
+                                class="btn btn-submit"><i class="fa-solid fa-pen-to-square"></i> Editar dados</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script lang="ts">
@@ -23,5 +49,4 @@ export default class Perfil extends Vue {
 
 </script>
 
-<style lang="scss">
-</style>
+<style lang="scss"></style>
